2b68cabca43c7092c1bfe24372091ed34789d3fc
LIO720 <lbdronemediaservice@gmail.com>
2026-08-30 16:13:06 +0200

    Update match.js

--- api/match.js
+++ api/match.js
@@ -1,16 +1,18 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   const API_FOOTBALL_KEY = "15a5eabb2d17b76fb922cd808a574779";
-  const url = "https://v3.football.api-sports.io/fixtures?league=61&season=2026&team=85&next=1";
+  
+  // On interroge uniquement par équipe sans forcer de saison bloquée
+  const url = "https://v3.football.api-sports.io/fixtures?team=85&next=1";
 
   try {
     const response = await fetch(url, {
       headers: { "x-apisports-key": API_FOOTBALL_KEY }
     });
     const data = await response.json();
+    
     return res.status(200).json({
       statusHttp: response.status,
-      urlTestee: url,
       contenuApi: data
     });
   } catch (e) {
